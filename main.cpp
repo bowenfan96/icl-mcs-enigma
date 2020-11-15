65e03566542d517ec39dd9d16da726d16ef8a41a
bowenfan96 <fan.bowen20@imperial.ac.uk>
2020-11-15 18:59:40 +0800

    plugboard works

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 class plugboard {
 public:
+    plugboard(const char*);
     std::vector<int> map_from;
     std::vector<int> map_to;
+    
+    int mapper(int);
 };
 
 // plugboard constructor
@@ -33,10 +37,29 @@ plugboard::plugboard(const char* pb_filename) {
     
 }
 
+// plugboard mapper function
+int plugboard::mapper(int input) {
+    std::vector<int>::iterator iter = std::find(map_from.begin(), map_from.end(), input);
+    
+    if(iter != map_from.cend()) {
+        int index = std::distance(map_from.begin(), iter);
+        return map_to.at(index);
+    }
+    
+    else {
+        return input;
+    }
+}
+
+
 class rotor {
 public:
+    rotor(const char*);
     int map_to[26];
     std::vector<int> triggers;
+    
+    void rotate();
+    int mapper(int);
 };
 
 // rotor constructor
@@ -48,12 +71,12 @@ rotor::rotor(const char* rot_filename) {
     int i = 0;
     while(rot_file >> rot_num) {
         if(i <= 25) {
-            map_to[i] = rot_num
+            map_to[i] = rot_num;
         }
         else {
-            triggers.push_back(rot_num)
+            triggers.push_back(rot_num);
         }
-        i++
+        i++;
     }
     
     // Check input mapping is one to one, and all inputs are mapped
@@ -62,10 +85,27 @@ rotor::rotor(const char* rot_filename) {
     // Check rotor starting position
 }
 
+// rotor rotate function
+void rotor::rotate() {
+    int swap = map_to[0];
+    for(int i=0; i<25; i++) {
+        map_to[i] = map_to[i+1];
+    }
+    map_to[25] = swap;
+}
+
+// rotor mapper function
+int rotor::mapper(int input) {
+    return map_to[input];
+}
+
 class reflector {
 public:
+    reflector(const char*);
     int map_from[13];
     int map_to[13];
+    
+    int mapper(int);
 };
 
 // reflector constructor
@@ -92,12 +132,59 @@ reflector::reflector(const char* rf_filename) {
     
 }
 
+// reflector mapper function
+int reflector::mapper(int input) {
+    int index = std::distance(map_from, std::find(map_from, map_from + 13, input));
+    return map_to[index];
+}
+
+/* Press A
+ * plugboard makes A --> G
+ * rotor 1 makes A --> B default, and in default position 0 (not displaced)
+ * rotor 1 eg. BCDEFG
+ * rotor 1 gets turned
+ * now A --> C (CDEFGB)
+ * map_to[i] = map_to[i+1]
+ * 
+ * so rotor 1 in position 2 (displaced 3 notches)
+ * rotor 2 takes in F, unless rotor 1 displaces it
+ * e.g F --> X in rotor 2
+ * reflector maps X to N
+ * N goes into reverse mapping rotor
+ * 
+ * Reverse mapping rotor:
+ * search rotor index corresponding to the letter
+ * mapped to same alphabet index
+*/
+
+
 
 
 int main() {
-    plugboard pb;
-    pb.pb_importer("/home/bowen/ipl/mcslab_2_bf420/plugboards/V.pb");
-    for(int i=0; i<pb.map_from.size();i++) {
-        std::cout << pb.map_from[i] << std::endl;
-    }
+    
+    // load plugboard
+    plugboard pb("/home/bowen/ipl/mcslab_2_bf420/plugboards/V.pb");
+    
+    // create a vector of rotors
+    // std::vector<rotor> rotors;
+    
+    // get num_rotors from input parameter at the start
+    // int num_rotors;
+    // rotors.resize(num_rotors);
+    
+    // load rotors
+    
+    
+    // call plugboard with input integer representing letter
+    
+    // test with input = 21
+    // should output to 1
+    int input = 21;
+    
+    input = pb.mapper(input);
+    
+    std::cout << input << std::endl;
+    
+    
+    
 }
